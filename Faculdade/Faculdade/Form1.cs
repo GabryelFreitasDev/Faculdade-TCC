4a118fe4c9c0ffe0a1b561081c20506c2749ecb8
unknown <guilhermearaujop26@gmail.com>
2022-05-18 16:36:38 -0300

    Comitar arquivos estranhos

--- Faculdade/Faculdade/Form1.cs
+++ Faculdade/Faculdade/Form1.cs
@@ -23,5 +23,19 @@ namespace Faculdade
             curso.Inserir(textBox1.Text);
             MessageBox.Show(curso.mensagem);
         }
+
+        private void Btn_excluiCurso_Click(object sender, EventArgs e)
+        {
+            Curso curso = new Curso();
+            curso.Excluir(Txb_excluiCurso.Text);
+            MessageBox.Show(curso.mensagem);
+        }
+
+        private void Btn_Editar_Click(object sender, EventArgs e)
+        {
+            Curso curso = new Curso();
+            curso.Editar(Txb_nomeAlterar.Text, Txb_nomeNovo.Text);
+            MessageBox.Show(curso.mensagem);
+        }
     }
 }
