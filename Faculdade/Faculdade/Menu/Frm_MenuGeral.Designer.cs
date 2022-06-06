4992e40fdb4ca92adfb5d27dcfb5cd748bfe68d1
unknown <guilhermearaujop26@gmail.com>
2022-06-06 15:08:35 -0300

    Arrumando tela de inicio de novo, mais uma vez, novamente

--- Faculdade/Faculdade/Menu/Frm_MenuGeral.Designer.cs
+++ Faculdade/Faculdade/Menu/Frm_MenuGeral.Designer.cs
@@ -38,14 +38,22 @@ namespace Faculdade
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.AliareAcademy = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel4 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // Lbl_contadorCursos
@@ -54,10 +62,10 @@ namespace Faculdade
             this.Lbl_contadorCursos.AutoSize = true;
             this.Lbl_contadorCursos.BackColor = System.Drawing.Color.Transparent;
             this.Lbl_contadorCursos.Font = new System.Drawing.Font("Nirmala UI", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lbl_contadorCursos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(51)))), ((int)(((byte)(90)))));
-            this.Lbl_contadorCursos.Location = new System.Drawing.Point(39, 439);
+            this.Lbl_contadorCursos.ForeColor = System.Drawing.Color.White;
+            this.Lbl_contadorCursos.Location = new System.Drawing.Point(36, 176);
             this.Lbl_contadorCursos.Name = "Lbl_contadorCursos";
-            this.Lbl_contadorCursos.Size = new System.Drawing.Size(165, 128);
+            this.Lbl_contadorCursos.Size = new System.Drawing.Size(164, 128);
             this.Lbl_contadorCursos.TabIndex = 0;
             this.Lbl_contadorCursos.Text = "10";
             this.Lbl_contadorCursos.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -68,10 +76,10 @@ namespace Faculdade
             this.Lbl_contadorTurmas.AutoSize = true;
             this.Lbl_contadorTurmas.BackColor = System.Drawing.Color.Transparent;
             this.Lbl_contadorTurmas.Font = new System.Drawing.Font("Nirmala UI", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lbl_contadorTurmas.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(51)))), ((int)(((byte)(90)))));
-            this.Lbl_contadorTurmas.Location = new System.Drawing.Point(337, 439);
+            this.Lbl_contadorTurmas.ForeColor = System.Drawing.Color.White;
+            this.Lbl_contadorTurmas.Location = new System.Drawing.Point(40, 176);
             this.Lbl_contadorTurmas.Name = "Lbl_contadorTurmas";
-            this.Lbl_contadorTurmas.Size = new System.Drawing.Size(165, 128);
+            this.Lbl_contadorTurmas.Size = new System.Drawing.Size(164, 128);
             this.Lbl_contadorTurmas.TabIndex = 1;
             this.Lbl_contadorTurmas.Text = "10";
             this.Lbl_contadorTurmas.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -82,10 +90,10 @@ namespace Faculdade
             this.Lbl_contadorAlunos.AutoSize = true;
             this.Lbl_contadorAlunos.BackColor = System.Drawing.Color.Transparent;
             this.Lbl_contadorAlunos.Font = new System.Drawing.Font("Nirmala UI", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lbl_contadorAlunos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(51)))), ((int)(((byte)(90)))));
-            this.Lbl_contadorAlunos.Location = new System.Drawing.Point(623, 439);
+            this.Lbl_contadorAlunos.ForeColor = System.Drawing.Color.White;
+            this.Lbl_contadorAlunos.Location = new System.Drawing.Point(38, 176);
             this.Lbl_contadorAlunos.Name = "Lbl_contadorAlunos";
-            this.Lbl_contadorAlunos.Size = new System.Drawing.Size(165, 128);
+            this.Lbl_contadorAlunos.Size = new System.Drawing.Size(164, 128);
             this.Lbl_contadorAlunos.TabIndex = 2;
             this.Lbl_contadorAlunos.Text = "10";
             this.Lbl_contadorAlunos.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -96,10 +104,10 @@ namespace Faculdade
             this.Lbl_contadorMaterias.AutoSize = true;
             this.Lbl_contadorMaterias.BackColor = System.Drawing.Color.Transparent;
             this.Lbl_contadorMaterias.Font = new System.Drawing.Font("Nirmala UI", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lbl_contadorMaterias.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(51)))), ((int)(((byte)(90)))));
-            this.Lbl_contadorMaterias.Location = new System.Drawing.Point(911, 439);
+            this.Lbl_contadorMaterias.ForeColor = System.Drawing.Color.White;
+            this.Lbl_contadorMaterias.Location = new System.Drawing.Point(42, 173);
             this.Lbl_contadorMaterias.Name = "Lbl_contadorMaterias";
-            this.Lbl_contadorMaterias.Size = new System.Drawing.Size(165, 128);
+            this.Lbl_contadorMaterias.Size = new System.Drawing.Size(164, 128);
             this.Lbl_contadorMaterias.TabIndex = 3;
             this.Lbl_contadorMaterias.Text = "10";
             this.Lbl_contadorMaterias.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -110,8 +118,8 @@ namespace Faculdade
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Nirmala UI", 16F, System.Drawing.FontStyle.Bold);
-            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(51)))), ((int)(((byte)(90)))));
-            this.label4.Location = new System.Drawing.Point(906, 396);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(36, 113);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(170, 60);
             this.label4.TabIndex = 7;
@@ -124,8 +132,8 @@ namespace Faculdade
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Nirmala UI", 16F, System.Drawing.FontStyle.Bold);
-            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(51)))), ((int)(((byte)(90)))));
-            this.label5.Location = new System.Drawing.Point(32, 396);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(28, 127);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(172, 60);
             this.label5.TabIndex = 8;
@@ -138,8 +146,8 @@ namespace Faculdade
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Nirmala UI", 16F, System.Drawing.FontStyle.Bold);
-            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(51)))), ((int)(((byte)(90)))));
-            this.label6.Location = new System.Drawing.Point(332, 396);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(32, 116);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(170, 60);
             this.label6.TabIndex = 9;
@@ -152,8 +160,8 @@ namespace Faculdade
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Nirmala UI", 16F, System.Drawing.FontStyle.Bold);
-            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(51)))), ((int)(((byte)(90)))));
-            this.label7.Location = new System.Drawing.Point(616, 396);
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(32, 116);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(172, 60);
             this.label7.TabIndex = 10;
@@ -164,30 +172,19 @@ namespace Faculdade
             // 
             this.AliareAcademy.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.AliareAcademy.BackColor = System.Drawing.Color.Transparent;
-            this.AliareAcademy.BackgroundImage = global::Faculdade.Properties.Resources.blob__1_;
+            this.AliareAcademy.BackgroundImage = global::Faculdade.Properties.Resources.aliareAcademy;
             this.AliareAcademy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.AliareAcademy.Location = new System.Drawing.Point(407, 83);
+            this.AliareAcademy.Location = new System.Drawing.Point(408, 35);
             this.AliareAcademy.Name = "AliareAcademy";
             this.AliareAcademy.Size = new System.Drawing.Size(307, 105);
             this.AliareAcademy.TabIndex = 11;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Image = global::Faculdade.Properties.Resources._57_Student_removebg_preview_2_;
-            this.pictureBox1.Location = new System.Drawing.Point(683, 343);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(51, 50);
-            this.pictureBox1.TabIndex = 12;
-            this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
             this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.Image = global::Faculdade.Properties.Resources.materias;
-            this.pictureBox2.Location = new System.Drawing.Point(974, 343);
+            this.pictureBox2.Location = new System.Drawing.Point(99, 38);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(51, 50);
             this.pictureBox2.TabIndex = 13;
@@ -198,7 +195,7 @@ namespace Faculdade
             this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox3.Image = global::Faculdade.Properties.Resources.chapeuFormatura;
-            this.pictureBox3.Location = new System.Drawing.Point(87, 353);
+            this.pictureBox3.Location = new System.Drawing.Point(90, 38);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(51, 50);
             this.pictureBox3.TabIndex = 14;
@@ -209,42 +206,96 @@ namespace Faculdade
             this.pictureBox4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox4.Image = global::Faculdade.Properties.Resources.professorBnc1;
-            this.pictureBox4.Location = new System.Drawing.Point(393, 343);
+            this.pictureBox4.Location = new System.Drawing.Point(95, 38);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(51, 50);
             this.pictureBox4.TabIndex = 15;
             this.pictureBox4.TabStop = false;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.Lbl_contadorCursos);
+            this.panel1.Location = new System.Drawing.Point(41, 167);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(230, 315);
+            this.panel1.TabIndex = 16;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel2.Controls.Add(this.pictureBox4);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.Lbl_contadorAlunos);
+            this.panel2.Location = new System.Drawing.Point(317, 167);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(230, 315);
+            this.panel2.TabIndex = 17;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.Lbl_contadorTurmas);
+            this.panel3.Location = new System.Drawing.Point(592, 167);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(230, 315);
+            this.panel3.TabIndex = 18;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Faculdade.Properties.Resources._57_Student_removebg_preview_2_;
+            this.pictureBox1.Location = new System.Drawing.Point(99, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 50);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel4.Controls.Add(this.pictureBox2);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Controls.Add(this.Lbl_contadorMaterias);
+            this.panel4.Location = new System.Drawing.Point(860, 167);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(230, 315);
+            this.panel4.TabIndex = 19;
+            // 
             // Frm_menuGeral
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::Faculdade.Properties.Resources.bg_imagemTeste;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(51)))), ((int)(((byte)(90)))));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1120, 576);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.AliareAcademy);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.Lbl_contadorMaterias);
-            this.Controls.Add(this.Lbl_contadorAlunos);
-            this.Controls.Add(this.Lbl_contadorTurmas);
-            this.Controls.Add(this.Lbl_contadorCursos);
             this.DoubleBuffered = true;
             this.Name = "Frm_menuGeral";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -259,9 +310,13 @@ namespace Faculdade
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Panel AliareAcademy;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel4;
     }
 }
